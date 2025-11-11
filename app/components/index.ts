5a59f145df8d4d41bef5fb79264f682b5d129cca
임승현 <96limshyun@gmail.com>
2025-11-11 21:04:56 +0900

    chore: 컴포넌트 export 업데이트
    - app/components: 새로운 섹션 컴포넌트 export 추가
    - libs/design-system: TextArea 컴포넌트 export 추가

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,2 +1,5 @@
 export { default as AdditionalImagesUpload } from "./AdditionalImagesUpload";
 export { default as MainImageUpload } from "./MainImageUpload";
+export { default as CategorySection } from "./CategorySection";
+export { default as ContentTitleSection } from "./ContentTitleSection";
+export { default as ActivityTypeSection } from "./ActivityTypeSection";
