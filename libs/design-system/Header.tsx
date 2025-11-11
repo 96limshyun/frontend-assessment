d72d6b5561e6871e34a4df819ddff6dcd651d64d
임승현 <96limshyun@gmail.com>
2025-11-11 17:51:50 +0900

    feat: add header layout component

---
+++ libs/design-system/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className="flex h-[64px] w-full items-center justify-center border-b border-[#D7D7D7]">
+      <div className="relative flex h-full w-[360px] items-center justify-between px-5 md:w-[1100px]">
+        <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-2xl leading-[130%] tracking-[-0.02em]">
+          과제
+        </h1>
+        <div className="ml-auto flex h-[38px] w-[120px] items-center justify-center rounded-[4px] bg-[#D7D7D7] px-3 py-2 text-center">
+          <span className="text-base font-semibold leading-[140%] tracking-[-0.02em] text-white">
+            다음으로
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ libs/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as twc } from "./twc";
