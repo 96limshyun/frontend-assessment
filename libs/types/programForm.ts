3abd4535d986931c3336551eb2d11fd849ed9389
임승현 <96limshyun@gmail.com>
2025-11-14 18:57:40 +0900

    feat: ProgramFormState와 SessionInfo에 대한 zod 스키마 정의 추가

--- libs/types/programForm.ts
+++ libs/types/programForm.ts
@@ -1,12 +1,28 @@
-import { SessionInfo } from "./sectionInfo";
+import { SessionInfoSchema } from "./sectionInfo";
+import { z } from "zod";
+import {
+  CONTENT_TITLE_MIN_LENGTH,
+  CONTENT_TITLE_MAX_LENGTH,
+} from "@/app/constants";
 
-export type ActivityType = "online" | "offline";
-
-export interface ProgramFormState {
-  mainImage: File | null;
-  additionalImages: File[];
-  categories: string[];
-  contentTitle: string;
-  activityType: ActivityType | null;
-  sessionInfo: SessionInfo[];
+export enum ActivityType {
+  ONLINE = "online",
+  OFFLINE = "offline",
 }
+
+export const ProgramFormStateSchema = z.object({
+  mainImage: z.instanceof(File).nullable(),
+  additionalImages: z.array(z.instanceof(File)),
+  categories: z.array(z.string()),
+  contentTitle: z
+    .string()
+    .min(CONTENT_TITLE_MIN_LENGTH)
+    .max(CONTENT_TITLE_MAX_LENGTH)
+    .refine((value) => value.trim() !== "", {
+      message: `${CONTENT_TITLE_MIN_LENGTH}자 이상 입력해주세요.`,
+    }),
+  activityType: z.nativeEnum(ActivityType).nullable(),
+  sessionInfo: z.array(SessionInfoSchema),
+});
+
+export type ProgramFormState = z.infer<typeof ProgramFormStateSchema>;
--- libs/types/sectionInfo.ts
+++ libs/types/sectionInfo.ts
@@ -1,15 +1,29 @@
 export type SessionPeriod = "오전" | "오후";
 
-export interface TimeInfo {
-  period: SessionPeriod;
-  hour: string;
-  minute: string;
-}
+import { z } from "zod";
+import {
+  ACTIVITY_CONTENT_MIN_LENGTH,
+  ACTIVITY_CONTENT_MAX_LENGTH,
+} from "@/app/constants";
 
-export interface SessionInfo {
-  sessionId: string;
-  sessionDate: Date | null;
-  sessionTimeStart: TimeInfo;
-  sessionTimeEnd: TimeInfo;
-  activityContent: string;
-}
+export const TimeInfoSchema = z.object({
+  period: z.enum(["오전", "오후"]),
+  hour: z.string(),
+  minute: z.string(),
+});
+
+export const SessionInfoSchema = z.object({
+  sessionId: z.string(),
+  sessionDate: z.date().nullable(),
+  sessionTimeStart: TimeInfoSchema,
+  sessionTimeEnd: TimeInfoSchema,
+  activityContent: z
+    .string()
+    .min(ACTIVITY_CONTENT_MIN_LENGTH)
+    .max(ACTIVITY_CONTENT_MAX_LENGTH)
+    .refine((value) => value.trim() !== "", {
+      message: `${ACTIVITY_CONTENT_MIN_LENGTH}자 이상 입력해주세요.`,
+    }),
+});
+
+export type SessionInfo = z.infer<typeof SessionInfoSchema>;
